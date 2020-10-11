5650c5a525fe80b87fa89d0626a50a7a09d81960
Ismael Suarez <spawn@Ismaels-MacBook-Pro.local>
2020-10-11 12:01:07 -0500

    diceGame

--- week3/dice-roll/src/Dice.js
+++ week3/dice-roll/src/Dice.js
@@ -16,6 +16,10 @@ class Dice extends Component {
         
     }
 
+    KeepDie = (die) => {
+        let dieArray = _.concat(this.state.dieSelected, die)
+            this.setState({dieSelected: dieArray})
+    }
     DieRoll = () => {
         const rollDice = () => _.random(1, 6) 
         const diceReset = this.state.dieResetsIn
@@ -30,55 +34,55 @@ class Dice extends Component {
                         dieResetsIn: 3
                 })
             } else {
-                // switch(!_.includes(this.state.dieSelected)) {
-                //     case 'Die One':
-                //         this.setState({dieOne: rollDice()})
-                //         break;
-                //     case 'Die Two':
-                //         this.setState({dieTwo: rollDice()})
-                //         break;
-                //     case 'Die Three':
-                //         this.setState({dieThree: rollDice()})
-                //         break;
-                //     case 'Die Four':
-                //         this.setState({dieFour: rollDice()})
-                //         break;
-                //     case 'Die Five':
-                //         this.setState({dieFive: rollDice()})
-                //         break;
-                //     default:
-                //         console.error('error')
-                // }   
+                
+                // const selected = _.forEach(die => {
+            //         switch(!_.includes(die.state.dieSelected)) {
+            //             case 'Die One':
+            //                 this.setState({dieOne: rollDice()})
+            //                 break;
+            //             case 'Die Two':
+            //                 this.setState({dieTwo: rollDice()})
+            //                 break;
+            //             case 'Die Three':
+            //                 this.setState({dieThree: rollDice()})
+            //                 break;
+            //             case 'Die Four':
+            //                 this.setState({dieFour: rollDice()})
+            //                 break;
+            //             case 'Die Five':
+            //                 this.setState({dieFive: rollDice()})
+            //                 break;
+            //             default:
+            //                 console.error('error')
+            //                 break;
+            //         } 
+                    
+            //         return (<DieDisplay state={this.state} itemSelected={this.KeepDie} />
+            //         )
+            // })}}
 
-
+                
                 if(!_.includes(this.state.dieSelected, 'Die One')) 
                     {this.setState({dieOne: rollDice()})}
-
                 if(!_.includes(this.state.dieSelected, 'Die Two')) 
                     {this.setState({dieTwo: rollDice()})}
-
                 if(!_.includes(this.state.dieSelected, 'Die Three')) 
                     {this.setState({dieThree: rollDice()})}
-
                 if(!_.includes(this.state.dieSelected,  'Die Four')) 
                     {this.setState({dieFour: rollDice()})}
-
                 if(!_.includes(this.state.dieSelected, 'Die Five')) 
-                    {this.setState({dieFive: rollDice()})}
-                
-                    this.setState({dieResetsIn: diceReset - 1})
+                    {this.setState({dieFive: rollDice()})
+                this.setState({dieResetsIn: diceReset - 1})
             }
+        }
     }
 
-    KeepDie = (die) => {
-        let dieArray = _.concat(this.state.dieSelected, die)
-            this.setState({dieSelected: dieArray})
-    }
+    
 
     render() { 
         return (
             <div className='dieContainer'>
-                <DieDisplay state={this.state} itemSelected={this.KeepDie} />
+                <DieDisplay state={this.state} itemSelected={this.KeepDie}/>
                 <h3>Die Resets in ... {this.state.dieResetsIn}</h3>
                 <button onClick={this.DieRoll}>Roll The Dice</button>
             </div>
